9b86e2773c607129114836c810e6bafc9dfd9893
kingofthebongo2008 <stefan.dyulgerov@gmail.com>
2017-05-17 15:24:03 +0300

    added two shaders + root signature

---
+++ src/gim_viewer/gpu_code/bunny_pixel.hlsl
@@ -0,0 +1,34 @@
+
+struct interpolants
+{
+    float4 position     : SV_POSITION0;
+    float2 uv           : texcoord0;
+};
+
+float checker_board_pattern(float2 uv)
+{
+    float2 c = floor(uv) / 2;
+    return frac(c.x + c.y) * 2;
+}
+
+float4 checker_board(float2 uv)
+{
+    float2 uv_scaled = uv * float2(32.0f, 32.0f);
+    float checker = checker_board_pattern(uv_scaled);
+    return float4(checker, checker, checker, 1.0f);
+}
+
+SamplerState g_linear       : register(s0)
+{
+    Filter = MIN_MAG_MIP_LINEAR;
+    AddressU = Clamp;
+    AddressV = Clamp;
+};
+
+
+#include "root_signature.h"
+[RootSignature( MyRS1 ) ]
+float4 main( interpolants r ) : SV_Target0
+{
+    return checker_board(r.uv);
+}
